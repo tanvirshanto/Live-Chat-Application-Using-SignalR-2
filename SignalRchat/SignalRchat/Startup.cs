498e51e7b41ab83fa3801e50281432c1279d723f
shantotanvir <tanvirrahman.aiub@gmail.com>
2017-07-18 17:32:56 +0600

    first commit

---
+++ SignalRchat/SignalRchat/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SignalRchat.Startup))]
+namespace SignalRchat
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // Any connection or hub wire up and configuration should go here
+            app.MapSignalR();
+        }
+    }
+}
